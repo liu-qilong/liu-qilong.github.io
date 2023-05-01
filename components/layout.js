cd421b454916d2bd4606b628b001f9f76e031189
KNPOB <qilong-kirov.liu@outlook.com>
2023-05-01 16:01:49 +0800

    unifiied layout width management
    replace tailwind md:basis-1/3 and md:basis-2/3 with css max-width

--- components/layout.js
+++ components/layout.js
@@ -3,14 +3,14 @@ import Navigator from './navigator'
 export default function Layout({ children }) {
     return (
         <div className="flex flex-col md:flex-row">
-            {/* navigator md:sticky */}
-            <div className="mx-auto text-center pt-5 md:pt-20 md:pr-20 md:pl-20
-                md:basis-1/3 md:h-screen md:sticky md:top-0">
+            {/* navigator */}
+            <div className="navigator mx-auto text-center pt-5 md:pt-20 md:pr-20 md:pl-20
+                md:h-screen md:sticky md:top-0">
                 <Navigator/>
             </div>
 
             {/* content */}
-            <div className="content w-screen md:basis-2/3 p-5 md:p-20 pt-0 md:pt-10">
+            <div className="content w-screen p-5 md:p-20 pt-0 md:pt-10">
                 {children}
             </div>
         </div>
