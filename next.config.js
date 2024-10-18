f649005abefe96ac8967432563429d07319be07f
KNPOB <qilong-kirov.liu@outlook.com>
2024-10-18 20:01:46 +0800

    refine

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/qilong-liu",
+  basePath: "/qilong-liu/public",
   images: {
     unoptimized: true,
   },
