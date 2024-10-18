e7d22e4af2ba0fb286fa8b893ca8374216a6ef64
KNPOB <qilong-kirov.liu@outlook.com>
2024-10-18 19:54:11 +0800

    github action

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/qilong-liu",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   transpilePackages: ['react-tweet']
 }
