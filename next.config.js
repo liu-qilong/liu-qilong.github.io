6a4d236e53b50366b47fd31c8db81f16a6d9d81b
KNPOB <qilong-kirov.liu@outlook.com>
2024-10-18 20:42:23 +0800

    try to fix img path issue

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/qilong-liu/public",
+  basePath: "/qilong-liu",
   images: {
+    path: `${basePath}/public`,
     unoptimized: true,
   },
   reactStrictMode: true,
